fa639da7d90ab8c9e090c2a1fbc491a54086e0f2
Camille Franklin <spacejamilli20@gmail.com>
2021-05-05 01:18:12 -0500

    Cannot find module 'express' from 'api/server.js' TEST ERROR

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,21 @@
 // implement your posts router here
+
+const router = require('express').Router();
+const Post = require('./posts-model');
+
+router.get('/', (req, res) => {
+    Post.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'There was an error retrieving the posts'
+            });
+        });
+});
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
+const express = require('express');
+const server = express();
 // require your posts router and connect it here
+const postsRouter = require('./posts/posts-router');
+
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+server.use('*', (req, res) => {
+    res.status(404).send(`
+        <h2>mortal kombat</h2> 
+    `);
+});
+
+module.exports = server;
